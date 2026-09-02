9c06720684b0258abaf3eee5fc8768e6746e2f00
mekotio <147532138+IceStorm1k@users.noreply.github.com>
2026-09-02 16:57:04 +0500

    Change axios base URL to production endpoint

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4444",
+  baseURL: "https://mern-pro-13ek.onrender.com",
 });
 
 instance.interceptors.request.use((config) => {
